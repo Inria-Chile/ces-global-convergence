9d1d5f1333ff1320de56afe5b72eb70f1d426bae
Matias Neto <matias.neto@inria.cl>
2026-03-25 13:01:42 -0300

    style: reformat

--- plot_benchmark_convergence.py
+++ plot_benchmark_convergence.py
@@ -82,7 +82,6 @@ def plot_from_history(df,d,case):
 
     if not os.path.exists("figures"):
         os.makedirs("figures")
-    
     plt.savefig(f'figures/final_convergence_d{d}_{case}.pdf', dpi=300)
     #plt.show()
 
